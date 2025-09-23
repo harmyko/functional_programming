fca568f0f3a3e9698150fc819d44bf8c517097a0
harmyko <ugniusteiserskis2005@gmail.com>
2025-09-23 21:03:20 +0000

    defined types and basic examples

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -2,15 +2,68 @@ module Lib1
     ( examples, Command(..), Dumpable(..)
     ) where
 
-data Dumpable = Examples
+data Vehicle = Vehicle
+  { vehiclePlate :: String
+  , driver :: Driver
+  , passengers :: [Passenger]
+  } deriving Show
+
+data Driver = Driver
+  { driverSex :: String
+  , driverAge :: Int
+  } deriving Show
+
+data Passenger = Passenger
+  { passengerSex :: String
+  , passengerAge :: Int
+  } deriving Show
+
+data Command
+  = AddVehicle Vehicle
+  | FilterByPlate String
+  | Sequence Command Command
+  | Dump Dumpable
   deriving Show
 
--- This is a "root" ADT representing your grammar,
--- Please expand this ADT as needed
-data Command = Dump Dumpable
+data Dumpable 
+  = Examples
   deriving Show
+
+vehicle0 :: Vehicle
+vehicle0 = Vehicle
+  { vehiclePlate = "ABC-123"
+  , driver = Driver { driverSex = "M", driverAge = 35 }
+  , passengers =
+      [ ]
+  }
+
+vehicle1 :: Vehicle
+vehicle1 = Vehicle
+  { vehiclePlate = "XYZ-789"
+  , driver = Driver { driverSex = "F", driverAge = 28 }
+  , passengers =
+      [ Passenger { passengerSex = "M", passengerAge = 40 }
+      , Passenger { passengerSex = "F", passengerAge = 25 }
+      ]
+  }
+
+vehicle2 :: Vehicle
+vehicle2 = Vehicle
+  { vehiclePlate = "LMN-456"
+  , driver = Driver { driverSex = "M", driverAge = 50 }
+  , passengers =
+      [ Passenger { passengerSex = "F", passengerAge = 22 } ]
+  }
+
+example0 :: Command
+example0 = Sequence
+  (AddVehicle vehicle0)
+  (AddVehicle vehicle1) 
+
+example1 :: Command
+example1 = Sequence
+  example0
+  (AddVehicle vehicle2)
 
 examples :: [Command]
-examples = [
-    Dump Examples
-    ]
+examples = [example0, example1, Dump Examples]
